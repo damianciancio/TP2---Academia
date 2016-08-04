cc72d840cb65dc50176b088ebd1027e029495df4
Damian <damian.ciancio@yahoo.com.ar>
2016-08-04 18:27:53 -0300

    Se termino comision adapter

--- Data.Database/Data.Database/ComisionAdapter.cs
+++ Data.Database/Data.Database/ComisionAdapter.cs
@@ -52,5 +52,134 @@ namespace Data.Database
             }
             return comisiones;
         }      
+
+        public Comision GetOne(int ID)
+        {
+            Comision co = new Comision();
+            try
+            {
+                this.OpenConnection();
+                SqlCommand cmdMaterias = new SqlCommand("select * from comisiones " +
+                    "inner join planes on planes.id_plan = comisiones.id_plan " +
+                    "inner join especialidades on especialidades.id_especialidad = planes.id_especialidad "+
+                    "where comision.id_comision = @id", SqlConn);
+                cmdMaterias.Parameters.Add("@id", SqlDbType.Decimal).Value = ID;
+                SqlDataReader drComisiones = cmdMaterias.ExecuteReader();
+                if (drComisiones.Read())
+                {
+                    Plan pl = new Plan();
+                    Especialidad es = new Especialidad();
+                    co.ID = (int)drComisiones["comisiones.id_comision"];
+                    co.DescComision = (string)drComisiones["desc_comision"];
+                    co.AnioEspecialidad = (int)drComisiones["anio_especialidad"];
+                    co.IdPlan = (int)drComisiones["comisiones.id_plan"];
+                    pl.ID = (int)drComisiones["planes.id_plan"];
+                    pl.Descripcion = (string)drComisiones["planes.desc_plan"];
+                    es.ID = (int)drComisiones["especialidades.id_especialidad"];
+                    es.Descripcion = (string)drComisiones["especialidades.desc_especialidad"];
+                    pl.Especialidad = es;
+                    co.Plan = pl;
+                }
+                drComisiones.Close();
+            }
+            catch (Exception Ex)
+            {
+                throw new Exception("Error al recuperar de la base de datos", Ex);
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+            return co;
+        }
+
+        public void Delete(int ID)
+        {
+            try
+            {
+                this.OpenConnection();
+                SqlCommand cmdDelete = new SqlCommand("delete comisiones where id_comision = @id", SqlConn);
+                cmdDelete.Parameters.Add("@id", SqlDbType.Decimal).Value = ID;
+                cmdDelete.ExecuteNonQuery();
+            }
+            catch (Exception Ex)
+            {
+                throw new Exception ("No se puede eliminar, usuario mal intencionado",Ex);
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+        }
+        public void Save(Comision comision)
+        {
+            if (comision.State == BusinessEntity.States.New)
+            {
+                try
+                {
+                    this.Insert(comision);
+                }
+                catch (Exception Ex)
+                {
+                    throw Ex;
+                }
+            }
+            else if (comision.State == BusinessEntity.States.Deleted)
+            {
+                this.Delete(comision.ID);
+            }
+            else if (comision.State == BusinessEntity.States.Modified)
+            {
+                this.Update(comision);
+            }
+            comision.State = BusinessEntity.States.Unmodified;
+        }
+        public void Update(Comision comision)
+        {
+            try
+            {
+                this.OpenConnection();
+                SqlCommand cmdComision = new SqlCommand("update comisiones set "+
+                    "comisiones.desc_comision = @descComision, comisiones.anio_especialidad =@anioEspecialidad"+
+                    "comisiones.id_plan = @idPlan where comisiones.id_comision = @id", SqlConn);
+                cmdComision.Parameters.Add("@id", SqlDbType.Decimal).Value = comision.ID;
+                cmdComision.Parameters.Add("@descComision", SqlDbType.VarChar).Value = comision.DescComision;
+                cmdComision.Parameters.Add("@anioEspecialidad", SqlDbType.Decimal).Value = comision.AnioEspecialidad;
+                cmdComision.Parameters.Add("@idPlan", SqlDbType.Decimal).Value = comision.Plan.ID;
+                cmdComision.ExecuteNonQuery();
+            }
+            catch (Exception Ex)
+            {
+                throw new Exception("No se pudo actualizar la comision", Ex);
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+        }
+
+        public void Insert(Comision co)
+        {
+            try
+            {
+                this.OpenConnection();
+                SqlCommand cmdInsert = new SqlCommand("insert comisiones (desc_comision, anio_especialidad, id_plan) " +
+                "values (@descComision, @anioEspecialidad, @idPlan) select @@identity", SqlConn);
+                cmdInsert.Parameters.Add("@id", SqlDbType.Decimal).Value = co.ID;
+                cmdInsert.Parameters.Add("@descComision", SqlDbType.VarChar).Value = co.DescComision;
+                cmdInsert.Parameters.Add("@anioEspecialidad", SqlDbType.Decimal).Value = co.AnioEspecialidad;
+                cmdInsert.Parameters.Add("@idPlan", SqlDbType.Decimal).Value = co.Plan.ID;
+                co.ID = Decimal.ToInt32((decimal)cmdInsert.ExecuteScalar());
+            }
+            catch (Exception Ex)
+            {
+                throw new Exception ("No se pudo insertar en la base de datos", Ex);
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+        }
+
     }
 }
