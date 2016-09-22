ac4388f78c5aeec21254dd7f798c542eb0e4e7f6
fernando mercado <fernandoalexis18@hotmail.com>
2016-09-22 11:04:00 -0300

    hjkiuuy

---
+++ Data.Database/Data.Database/TipoPersonaAdapter.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Entities;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Data.Database
+{
+    public class TipoPersonaAdapter : Adapter
+    {
+        public List<TipoPersona> GetAll()
+        {
+            List<TipoPersona> tiposPersona = new List<TipoPersona>();
+
+            try
+            {
+                this.OpenConnection();
+                SqlCommand cmdTiposPersona = new SqlCommand("select * from tipo_persona", SqlConn);
+                SqlDataReader drTiposPersona = cmdTiposPersona.ExecuteReader();
+                while (drTiposPersona.Read())
+                {
+                    TipoPersona tp = new TipoPersona();
+                    tp.ID = (int)drTiposPersona["id_tipo_persona"];
+                    tp.TipoPersonaDesc = (string)drTiposPersona["desc_tipo_persona"];
+                    tiposPersona.Add(tp);
+                }
+                drTiposPersona.Close();
+            }
+            catch (Exception Exc)
+            {
+                Exception ExcepcionManejada = new Exception("Error al recuperar los tipos de persona", Exc);
+                throw ExcepcionManejada;
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+
+            return tiposPersona;
+        }
+
+        public TipoPersona GetOne(int ID)
+        {
+            TipoPersona tp = new TipoPersona();
+            try
+            {
+                this.OpenConnection();
+                SqlCommand cmdTiposPersona = new SqlCommand("select * from tipo_persona where id_tipo_persona = @id", SqlConn);
+                cmdTiposPersona.Parameters.Add("@id", SqlDbType.Int).Value = ID;
+                SqlDataReader drTiposPersona = cmdTiposPersona.ExecuteReader();
+                if (drTiposPersona.Read())
+                {
+                   tp.ID = (int)drTiposPersona["tipo_persona.id_tipo_persona"];
+                   tp.TipoPersonaDesc = (string)drTiposPersona["tipo_persona.desc_tipo_persona"];
+                }
+            }
+            catch (Exception Exc)
+            {
+                Exception ExcepcionManejada = new Exception("No se pudo conectar", Exc);
+                throw ExcepcionManejada;
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+            return tp;
+        }
+
+        public void Delete(int ID)
+        {
+            try
+            {
+                this.OpenConnection();
+                SqlCommand cmdDelete = new SqlCommand("delete tipo_persona where id_tipo_persona=@id", SqlConn);
+                cmdDelete.Parameters.Add("@id", SqlDbType.Int).Value = ID;
+                cmdDelete.ExecuteNonQuery();
+            }
+            catch (Exception Ex)
+            {
+                Exception ExcepcionManejada = new Exception("No se puede eliminar, usuario malintencionado", Ex);
+                throw ExcepcionManejada;
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+        }
+
+        public void Save(TipoPersona tpersona)
+        {
+
+            if (tpersona.State == BusinessEntity.States.New)
+            {
+                try
+                {
+                    this.Insert(tpersona);
+                }
+                catch (Exception Ex)
+                {
+                    throw Ex;
+                }
+            }
+            else if (tpersona.State == BusinessEntity.States.Deleted)
+            {
+                this.Delete(tpersona.ID);
+            }
+            else if (tpersona.State == BusinessEntity.States.Modified)
+            {
+                this.Update(tpersona);
+            }
+           tpersona.State = BusinessEntity.States.Unmodified;
+        }
+
+        public void Update(TipoPersona pe)
+        {
+            try
+            {
+                this.OpenConnection();
+                SqlCommand cmdPersona = new SqlCommand("update tipo_persona set desc_tipo_persona = @desc where id_tipo_persona = @id", SqlConn);
+                cmdPersona.Parameters.Add("@desc", SqlDbType.VarChar, 50).Value = pe.TipoPersonaDesc;
+                cmdPersona.Parameters.Add("@id", SqlDbType.Decimal).Value = pe.ID;
+                cmdPersona.ExecuteNonQuery();
+            }
+            catch (Exception Ex)
+            {
+                Exception ExcepcionManejada = new Exception("No se pudo actualizar la materia", Ex);
+                throw ExcepcionManejada;
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+        }
+
+        public void Insert(TipoPersona pe)
+        {
+            try
+            {
+                this.OpenConnection();
+                SqlCommand cmdInsert = new SqlCommand("insert tipo_persona (desc_tipo_persona) " +
+                "values (@desc) select @@identity", SqlConn);
+                cmdInsert.Parameters.Add("@desc", SqlDbType.Decimal).Value = pe.TipoPersonaDesc;
+                pe.ID = Decimal.ToInt32((decimal)cmdInsert.ExecuteScalar());
+            }
+            catch (Exception Ex)
+            {
+                Exception ExcepcionManejada = new Exception("No se pudo crear nuevo Tipo Persona", Ex);
+                throw ExcepcionManejada;
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+        }
+    }
+}
---
+++ UI.Desktop/PersonaDesktop.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Business.Entities;
+using Negocio;
+
+namespace UI.Desktop
+{
+    public partial class PersonaDesktop : ApplicationForm
+    {
+      
+
+        public Persona PersonaActual
+        {
+            get;set;
+        }
+
+        public PersonaDesktop(ModoForm md) : this()
+        {
+            this.Modo = md;
+        }
+        public PersonaDesktop(int ID, ModoForm md) : this()
+        {
+           /* this.Modo = md;
+            PersonaLogic pl = new PersonaLogic();
+            PersonaActual = pl.GetOne(ID);
+            MapearDeDatos();*/
+        }
+
+        public override void MapearDeDatos()
+        {
+            this.txtNombre.Text = PersonaActual.Nombre;
+            this.txtApellido.Text = PersonaActual.Apellido;
+            this.txtDireccion.Text = PersonaActual.Direccion;
+            this.txtMail.Text = PersonaActual.Email;
+            this.txtLegajo.Text = PersonaActual.Legajo.ToString();
+            this.dtpFecNac.Value = PersonaActual.FechaNac;
+
+            switch (this.Modo)
+            {
+                case ModoForm.Alta:
+                    btnAceptar.Text = "Guardar";
+                    break;
+                case ModoForm.Baja:
+                    btnAceptar.Text = "Eliminar";
+                    break;
+                case ModoForm.Modificacion:
+                    btnAceptar.Text = "Guardar";
+                    break;
+                case ModoForm.Consulta:
+                    btnAceptar.Text = "Aceptar";
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        public override void MapearADatos()
+        {
+            if (this.Modo == ModoForm.Alta)
+            {
+                PersonaActual = new Persona();
+            }
+            else
+            {
+
+               PersonaActual.ID = int.Parse(txtID.Text);
+            }
+
+            PersonaActual.Nombre = this.txtNombre.Text;
+            PersonaActual.Apellido = this.txtApellido.Text;
+            PersonaActual.Direccion =this.txtDireccion.Text;
+            PersonaActual.Email = this.txtMail.Text;
+            int a;
+            if(int.TryParse(this.txtLegajo.Text, out a))
+            {
+                PersonaActual.Legajo = a;
+            }
+            else
+	        {
+                throw new Exception("Ingrese legajo válido"); 
+	        }
+            PersonaActual.FechaNac = this.dtpFecNac.Value;
+
+            switch (this.Modo)
+            {
+                case ModoForm.Alta:
+                    PersonaActual.State = BusinessEntity.States.New;
+                    break;
+                case ModoForm.Baja:
+                    PersonaActual.State = BusinessEntity.States.Deleted;
+                    break;
+                case ModoForm.Modificacion:
+                    PersonaActual.State = BusinessEntity.States.Modified;
+                    break;
+                case ModoForm.Consulta:
+                    PersonaActual.State = BusinessEntity.States.Unmodified;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+
+        public PersonaDesktop() : base ()
+        {
+            InitializeComponent();
+
+            List<TipoPersona> _listTipoPersona = new List<TipoPersona>();
+            _listTipoPersona.Add(new TipoPersona());
+            _listTipoPersona.AddRange(new TipoPersonaLogic().GetAll());
+
+            cmbTipoPersona.DataSource = _listTipoPersona;
+            cmbTipoPersona.DisplayMember = "TipoPersonaDesc";
+            cmbTipoPersona.ValueMember = "ID";
+
+            List<Especialidad> carreras = new List<Especialidad>();
+            carreras.Add(new Especialidad());
+            carreras.AddRange((new EspecialidadLogic()).GetAll());
+            cmbCarrera.DataSource = carreras;
+            cmbCarrera.DisplayMember = "Descripcion";
+            cmbCarrera.ValueMember = "ID";
+
+            actPlanes(0);
+
+
+        }
+
+        private void actPlanes(int idEspecialidad)
+        {
+            List<Plan> planes = new List<Plan>();
+            planes.Add(new Plan());
+            if (0 == idEspecialidad)
+            {
+                cmbPlan.Enabled = false;
+            }
+            else
+            {
+                planes.AddRange(new PlanLogic().GetAll(idEspecialidad));
+                cmbPlan.Enabled = true;
+            }
+
+            cmbPlan.DataSource = planes;
+            cmbPlan.DisplayMember = "Descripcion";
+            cmbPlan.ValueMember = "ID";
+
+        }
+
+        private void cmbCarrera_SelectedValueChanged(object sender, EventArgs e)
+        {
+            if (cmbCarrera.SelectedIndex != 0 )
+            {
+                actPlanes(((Especialidad)cmbCarrera.SelectedValue).ID);
+            }
+           
+        }
+
+       /* public override void GuardarCambios()
+        {
+            MapearADatos();
+            PersonaLogic esL = new PersonaLogic();
+            try
+            {
+                esL.Save(PersonaActual);
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show(Ex.Message);
+            }
+        }
+
+        private void actMaterias(int idComision)
+        {
+            List<Materia> _listMaterias = new List<Materia>();
+            _listMaterias.Add(new Materia());
+            if (0 == idComision)
+            {
+                cmbMaterias.Enabled = false;
+            }
+            else
+            {
+                _listMaterias.AddRange(new MateriaLogic().GetAll(/* TODO: Implementar un filtro mejor ));
+                cmbMaterias.Enabled = true;
+            }
+
+            cmbMaterias.DataSource = _listMaterias;
+            cmbMaterias.DisplayMember = "DescMateria";
+            cmbMaterias.ValueMember = "ID";
+
+        }
+
+        private void cmbComision_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int idComision = ((Comision)this.cmbComision.SelectedItem).ID;
+            actMaterias(idComision);
+
+            return;
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            this.GuardarCambios();
+            this.Close();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }*/
+    }
+}
